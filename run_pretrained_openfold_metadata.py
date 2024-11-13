80f1228b42bb07e04fa662d256e219d7931d3d43
etowahadams <etowahadams@gmail.com>
2024-11-13 12:16:42 -0500

    fix: deepspeed hanging issue

--- run_pretrained_openfold_metadata.py
+++ run_pretrained_openfold_metadata.py
@@ -22,6 +22,16 @@ import random
 import time
 import json
 
+import importlib
+
+if importlib.util.find_spec("deepspeed") is not None:
+    import deepspeed
+
+    # TODO: Resolve this
+    # This is a hack to prevent deepspeed from doing the triton matmul autotuning
+    # I'm not sure why it's doing this by default, but it's causing the tests to hang
+    deepspeed.HAS_TRITON = False
+
 logging.basicConfig()
 logger = logging.getLogger(__file__)
 logger.setLevel(level=logging.INFO)
